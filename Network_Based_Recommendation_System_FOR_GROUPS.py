2b5c166b0c08bfdf29d438764c361412e80c9930
sergio <sergioballesterossolanas@gmail.com>
2017-05-18 16:07:52 +0200

    first commit

---
+++ Network_Based_Recommendation_System_FOR_GROUPS.py
@@ -0,0 +1,95 @@
+import csv
+import time
+import pprint as pp
+import networkx as nx
+
+import Network_Based_Recommendation_System_FUNCTIONS as homework_2
+
+
+
+print
+print "Current time: " + str(time.asctime(time.localtime()))
+print
+print
+
+
+all_groups = [
+	{1701: 1, 1703: 1, 1705: 1, 1707: 1, 1709: 1}, ### Movie night with friends.
+	{1701: 1, 1702: 4}, ### First appointment scenario: the preferences of the girl are 4 times more important than those of the man.
+	{1701: 1, 1702: 2, 1703: 1, 1704: 2}, ### Two couples scenario: the preferences of girls are still more important than those of the men...
+	{1701: 1, 1702: 1, 1703: 1, 1704: 1, 1705: 1, 1720:10}, ### Movie night with a special guest.
+	{1701: 1, 1702: 1, 1703: 1, 1704: 1, 1705: 1, 1720:10, 1721:10, 1722:10}, ### Movie night with 3 special guests.
+]
+print
+pp.pprint(all_groups)
+print
+
+
+graph_file = "./input_data/u_data_homework_format.txt"
+
+pp.pprint("Load Graph.")
+print "Current time: " + str(time.asctime(time.localtime()))
+graph_users_items = homework_2.create_graph_set_of_users_set_of_items(graph_file)
+print " #Users in Graph= " + str(len(graph_users_items['users']))
+print " #Items in Graph= " + str(len(graph_users_items['items']))
+print " #Nodes in Graph= " + str(len(graph_users_items['graph']))
+print " #Edges in Graph= " + str(graph_users_items['graph'].number_of_edges())
+print "Current time: " + str(time.asctime(time.localtime()))
+print
+print
+
+
+pp.pprint("Create Item-Item-Weighted Graph.")
+print "Current time: " + str(time.asctime(time.localtime()))
+item_item_graph = homework_2.create_item_item_graph(graph_users_items)
+print " #Nodes in Item-Item Graph= " + str(len(item_item_graph))
+print " #Edges in Item-Item Graph= " + str(item_item_graph.number_of_edges())
+print "Current time: " + str(time.asctime(time.localtime()))
+print
+print
+
+
+### Conversion of the 'Item-Item-Graph' to a scipy sparse matrix representation.
+### This reduces a lot the PageRank running time ;)
+print
+print " Conversion of the 'Item-Item-Graph' to a scipy sparse matrix representation."
+N = len(item_item_graph)
+nodelist = item_item_graph.nodes()
+M = nx.to_scipy_sparse_matrix(item_item_graph, nodelist=nodelist, weight='weight', dtype=float)
+print " Done."
+print
+#################################################################################################
+
+
+output_file = open("./Output_Recommendation_for_Group.tsv", 'w')
+output_file_csv_writer = csv.writer(output_file, delimiter='\t', quotechar='"', quoting=csv.QUOTE_NONE)
+print
+for current_group in all_groups:
+	print "Current group: "
+	pp.pprint(current_group)
+	print "Current time: " + str(time.asctime(time.localtime()))
+	
+	sorted_list_of_recommended_items_for_current_group = []
+	# Your code here ;)
+	#
+	
+	
+	
+	
+	print "Recommended Sorted List of Items:"
+	print(str(sorted_list_of_recommended_items_for_current_group[:30]))
+	print
+	output_file_csv_writer.writerow(sorted_list_of_recommended_items_for_current_group)
+	
+output_file.close()	
+	
+	
+
+
+
+
+print
+print
+print "Current time: " + str(time.asctime(time.localtime()))
+print "Done ;)"
+print
---
+++ Network_Based_Recommendation_System_FUNCTIONS.py
@@ -0,0 +1,168 @@
+import csv
+import pprint as pp
+import networkx as nx
+import itertools as it
+import math
+import scipy.sparse
+import random
+
+
+
+
+def pagerank(M, N, nodelist, alpha=0.85, personalization=None, max_iter=100, tol=1.0e-6, dangling=None):
+	if N == 0:
+		return {}
+	S = scipy.array(M.sum(axis=1)).flatten()
+	S[S != 0] = 1.0 / S[S != 0]
+	Q = scipy.sparse.spdiags(S.T, 0, *M.shape, format='csr')
+	M = Q * M
+	
+	# initial vector
+	x = scipy.repeat(1.0 / N, N)
+	
+	# Personalization vector
+	if personalization is None:
+		p = scipy.repeat(1.0 / N, N)
+	else:
+		missing = set(nodelist) - set(personalization)
+		if missing:
+			#raise NetworkXError('Personalization vector dictionary must have a value for every node. Missing nodes %s' % missing)
+			print("")
+			print('Error: personalization vector dictionary must have a value for every node')
+			print("")
+			exit(-1)
+		p = scipy.array([personalization[n] for n in nodelist], dtype=float)
+		#p = p / p.sum()
+		sum_of_all_components = p.sum()
+		if sum_of_all_components > 1.001 or sum_of_all_components < 0.999:
+			print("")
+			print("Error: the personalization vector does not represent a probability distribution :(")
+			print("")
+			exit(-1)
+	
+	# Dangling nodes
+	if dangling is None:
+		dangling_weights = p
+	else:
+		missing = set(nodelist) - set(dangling)
+		if missing:
+			#raise NetworkXError('Dangling node dictionary must have a value for every node. Missing nodes %s' % missing)
+			print("")
+			print('Error: dangling node dictionary must have a value for every node.')
+			print("")
+			exit(-1)
+		# Convert the dangling dictionary into an array in nodelist order
+		dangling_weights = scipy.array([dangling[n] for n in nodelist], dtype=float)
+		dangling_weights /= dangling_weights.sum()
+	is_dangling = scipy.where(S == 0)[0]
+
+	# power iteration: make up to max_iter iterations
+	for _ in range(max_iter):
+		xlast = x
+		x = alpha * (x * M + sum(x[is_dangling]) * dangling_weights) + (1 - alpha) * p
+		# check convergence, l1 norm
+		err = scipy.absolute(x - xlast).sum()
+		if err < N * tol:
+			return dict(zip(nodelist, map(float, x)))
+	#raise NetworkXError('power iteration failed to converge in %d iterations.' % max_iter)
+	print("")
+	print('Error: power iteration failed to converge in '+str(max_iter)+' iterations.')
+	print("")
+	exit(-1)
+
+
+
+
+def create_graph_set_of_users_set_of_items(user_item_ranking_file):
+	graph_users_items = {}
+	all_users_id = set()
+	all_items_id = set()
+	g = nx.DiGraph()
+	input_file = open(user_item_ranking_file, 'r')
+	input_file_csv_reader = csv.reader(input_file, delimiter='\t', quotechar='"', quoting=csv.QUOTE_NONE)
+	for line in input_file_csv_reader:
+		user_id = int(line[0])
+		item_id = int(line[1])
+		rating = int(line[2])
+		g.add_edge(user_id, item_id, weight=rating)
+		all_users_id.add(user_id)
+		all_items_id.add(item_id)
+	input_file.close()
+	graph_users_items['graph'] = g
+	graph_users_items['users'] = all_users_id
+	graph_users_items['items'] = all_items_id
+	return graph_users_items
+	
+
+
+
+
+
+
+
+
+
+
+
+
+def create_item_item_graph(graph_users_items):
+	g = nx.Graph()
+	# Your code here ;)
+	
+	return g
+
+
+
+
+def create_preference_vector_for_teleporting(user_id, graph_users_items):
+	preference_vector = {}
+	# Your code here ;)
+	
+	
+	return preference_vector
+	
+
+
+
+def create_ranked_list_of_recommended_items(page_rank_vector_of_items, user_id, training_graph_users_items):
+	# This is a list of 'item_id' sorted in descending order of score.
+	sorted_list_of_recommended_items = []
+	# You can obtain this list from a list of [item, score] couples sorted in descending order of score.
+	
+	# Your code here ;)
+	
+	
+	return sorted_list_of_recommended_items
+
+
+
+
+def discounted_cumulative_gain(user_id, sorted_list_of_recommended_items, test_graph_users_items):
+	dcg = 0.
+	# Your code here ;)
+	
+	
+	return dcg
+	
+
+
+
+def maximum_discounted_cumulative_gain(user_id, test_graph_users_items):
+	dcg = 0.
+	# Your code here ;)
+	
+	
+	return dcg
+
+
+
+
+
+
+
+
+
+
+
+
+
